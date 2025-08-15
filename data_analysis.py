0e03a62ce8d639fd174d789f689f285afeb760a6
leon <kwame21@gmail.com>
2025-08-15 16:14:22 -0400

    modified

--- data_analysis.py
+++ data_analysis.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from scipy.stats import ttest_ind, chi2_contingency
+from scipy.stats import chi2_contingency
 
 # Load the Dataset
 url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv"
@@ -23,35 +21,6 @@ if p <= alpha:
 else:
     print("FailReject the null hypothesis: Variables are independent")
 
-
-
-
-# Visualize Distributions
-# sns.histplot(df["total_bill"], kde=True)
-# plt.title("Distribution of Total Bill")
-# plt.show()
-
-# Correlation heatmap
-# sns.heatmap(df.corr(), annot=True, cmap="coolwarm")
-# plt.title("Correlation Heatmap")
-# plt.show()
-
-# # Separate data by gender
-# male_tips = df[df['sex'] == 'Male']['tip']
-# female_tips = df[df['sex'] == 'Female']['tip']
-
-# # Perform t-test
-# t_stats, p_value = ttest_ind(male_tips, female_tips)
-# print("T-Statistic: ", t_stats)
-# print("P-Value: ", p_value)
-
-# # Interpret result
-# alpha = 0.05
-# if p_value <= alpha:
-#     print("Reject all null hypothesis: Siginificant difference")
-# else:
-#     print("Fail all null hypothesis: no siginficant difference")
-
 # # Difine variables
 X = df['total_bill'].values.reshape(-1, 1)
 y = df['tip'].values
@@ -65,13 +34,6 @@ print("Slope: ", model.coef_[0])
 print("Intercept: ", model.intercept_)
 print("R-Squared: ", model.score(X, y))
 
-# # Plot regression
-# sns.scatterplot(x=df['total_bill'], y=df['tip'], label='data', color='blue')
-# plt.plot(df['total_bill'], model.predict(X), color="red", label='Regression Line')
-# plt.title("Toatl bill vs Tips")
-# plt.xlabel("Total_Bill")
-# plt.ylabel("Tips")
-# plt.legend()
-# plt.show()
+
     
 
